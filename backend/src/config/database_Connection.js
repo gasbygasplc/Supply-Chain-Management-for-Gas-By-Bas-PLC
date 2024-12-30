1e18a64c11d74532440fa40fe44efe4aa9bb5471
Harishanth03 <kandeepanharishanth@gmail.com>
2024-12-30 21:44:19 +0530

    [Setup] Implement database connection and integrate with Express server

--- backend/src/config/database_Connection.js
+++ backend/src/config/database_Connection.js
@@ -1,5 +1,19 @@
-import momgoose from 'mongoose';
+import mongoose from "mongoose";
 
-const connectDatabase = async ( ) => {
-    
-};
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const connectDb = async () => {
+
+    console.log(process.env.DB_PASSWORD);
+
+    try
+    {
+        await mongoose.connect(`mongodb+srv://gasbygasplc:${process.env.DB_PASSWORD}@gasbygas.lrxn8.mongodb.net/?retryWrites=true&w=majority&appName=GasByGas`).then(() => console.log('DB connected'));
+    }
+    catch(error)
+    {
+        console.log('DB Connection Failed' , error.message);
+    }
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import cors from 'cors';
+import { connectDb } from './config/database_Connection.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 //===================================================== App Config =========================================================
 const app = express();
@@ -10,8 +13,13 @@ const PORT = 4000;
 app.use(express.json());
 app.use(cors());
 
+//===================================================== DB Config =========================================================
+
+connectDb();
+
 //===================================================== API Endpoints =======================================================
 
 app.get("/" , (request , response) => {response.send("API Working")});
 
 app.listen(PORT , () => console.log(`Server is running on port ${PORT}`));
+
