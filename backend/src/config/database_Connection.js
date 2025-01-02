faa8f8d22f5283eaddaf612fa36b54ee7319dfd9
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-02 20:40:05 +0530

    Refactor database connection logic and update environment variable usage

--- backend/src/config/database_Connection.js
+++ backend/src/config/database_Connection.js
@@ -1,17 +1,18 @@
 import mongoose from "mongoose";
 
-import dotenv from 'dotenv';
+export const databaseConnection = async() => {
 
-export const connectDb = async () => {
-
-    console.log(process.env.DB_PASSWORD);
-
-    try
+    try 
     {
-        await mongoose.connect(`mongodb+srv://gasbygasplc:${process.env.DB_PASSWORD}@gasbygas.lrxn8.mongodb.net/?retryWrites=true&w=majority&appName=GasByGas`).then(() => console.log('DB connected'));
-    }
-    catch(error)
+
+        await mongoose.connect(`mongodb+srv://gasbygasplc:${process.env.DB_PASSWORD}@gasbygas.lrxn8.mongodb.net/GasByGas`).then(() => console.log('Database Connected'));
+        
+    } 
+    catch (error)
     {
-        console.log('DB Connection Failed' , error.message);
+        console.log('Database Connection Failed' , error.message);
+        
     }
 }
+
+
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,11 @@
-import express from 'express';
-import cors from 'cors';
-import { connectDb } from './config/database_Connection.js';
+import express from 'express'; // this is the express js package
+
+import cors from 'cors'; // this use for cross origin request
+
+import  {databaseConnection}  from './config/database_Connection.js'; 
+
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 //===================================================== App Config =========================================================
@@ -10,16 +14,18 @@ const app = express();
 const PORT = 4000;
 
 //===================================================== Middlewares =========================================================
+
 app.use(express.json());
+
 app.use(cors());
 
 //===================================================== DB Config =========================================================
 
-connectDb();
+databaseConnection();
 
 //===================================================== API Endpoints =======================================================
 
-app.get("/" , (request , response) => {response.send("API Working")});
+app.get("/" , (request , response) => {response.send("Working")});
 
 app.listen(PORT , () => console.log(`Server is running on port ${PORT}`));
 
