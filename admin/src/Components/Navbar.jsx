a9013861d1d111fd9ee9fa0c4aedf0052e7b0d42
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-07 13:26:42 +0530

    [Feature] Implement logout functionality in Navbar component and enhance styling

--- admin/src/Components/Navbar.jsx
+++ admin/src/Components/Navbar.jsx
@@ -1,24 +1,38 @@
 import React, { useContext } from 'react';
-import { AdminContext } from '../Context/AdminContext';
 import { assets } from '../assets/assets';
+import { AdminContext } from '../Context/AdminContext';
+import {useNavigate} from 'react-router-dom'
 
 
 const Navbar = () => {
-  const { aToken } = useContext(AdminContext);
+
+  const { aToken ,SetAToken } = useContext(AdminContext);
+
+  const navigate = useNavigate();
+
+  const logout = () => 
+  {
+    navigate('/');
+
+    aToken && SetAToken("");
+
+    aToken && localStorage.removeItem('aToken')
+
+  }
 
   return (
 
     <div className='flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white'>
 
-        <div>
+        <div className='flex items-center gap-2 text-xs'>
 
-            <img src={assets.admin_logo} alt="" />
+            <img className='w-36 cursor-pointer' src={assets.admin_logo} alt="" />
 
-            <p>{aToken ? "Admin" : "Outlet"}</p>
+            <p className='border px-2.5 py-0.5 rounded-full border-gray-500'>{aToken ? "Admin" : "Outlet"}</p>
 
         </div>
 
-        <button>Signout</button>
+        <button onClick={logout} className='bg-primary-700 text-white text-sm px-10 py-3 rounded-lg'>Signout</button>
 
     </div>
 
