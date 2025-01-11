472859f6ef1b2164319797fcf4ef41566e28cbf6
mathumithasrikanthan <mathumithasrikanthan@gmail.com>
2025-01-11 19:50:25 +0530

    [Add] Implement Header component and integrate it into Home page

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import Header from '../Components/Header'
 
 const Home = () => {
     
   return (
-    <div>Home</div>
+
+    <div>
+      
+      <Header/>
+
+    </div>
+
   )
+
 }
 
 export default Home
