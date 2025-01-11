af3b2490b2d1a67c87547e4714fc0df9dcb2073e
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-11 20:31:56 +0530

    [Update] Refactor Header component structure and improve styling

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -6,17 +6,19 @@ const Header = () => {
 
     <div className="header">
         
-        <div className="header-contents">
+      <div className="header-contents">
 
-            <h2>Your Trusted Partner for Safe Gas Solutions!</h2>
+        <h2>Your Trusted Partner for Safe Gas Solutions!</h2>
 
-            <p>
-              Welcome to GasByGas, your one-stop destination for all gas cylinder needs. Order, refill, and receive cylinders at your doorstep, ensuring safety and quality every step of the way!
-            </p>
+        <p>
 
-            <button>View Menu</button>
+          Welcome to GasByGas, your one-stop destination for all gas cylinder needs. Order, refill, and receive cylinders at your doorstep, ensuring safety and quality every step of the way!
+          
+        </p>
 
-        </div>
+        <button>View Menu</button>
+
+      </div>
 
     </div>
 
