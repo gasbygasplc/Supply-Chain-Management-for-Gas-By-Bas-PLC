f8520902a5579b2b537029b967826c26d587ee1d
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-12 19:53:42 +0530

    Add gas stock model with schema for gas types and stock history

---
+++ backend/src/models/GasStockModels.js
@@ -0,0 +1,36 @@
+import mongoose, { STATES } from 'mongoose';
+
+const StockHistorySchema = new mongoose.Schema(
+
+    {
+
+        dateReceived: {type: Date, required: true, default: Date.now, },
+
+        quantity: { type: Number, required: true},
+
+    },
+
+    { _id: false }
+
+);
+
+const gasSchema = new mongoose.Schema({
+
+    type: {type:String , required: true , enum: ['Small' , 'Medium' , 'Large']},
+
+    weightKG : {type: Number , required: true},
+
+    image : {type : String , required : true},
+
+    price : {type : Number , required : true},
+
+    totalStock : {type: Number , default: 0},
+
+    stockHistroy : [StockHistorySchema] ,//i make it as an array
+
+
+} , {timestamps: true});
+
+const gasStockmodel = mongoose.models.gasStock || mongoose.model('gasStock', gasSchema);
+
+export default gasStockmodel;
