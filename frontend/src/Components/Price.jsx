d59e1447abf61b7dbf3f7ca0be7beecf28c2b1ee
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-12 15:02:47 +0530

    Refactor Price component to include dynamic pricing menu and improve layout

--- frontend/src/Components/Price.jsx
+++ frontend/src/Components/Price.jsx
@@ -1,47 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { asstets } from '../assets/Assets'
 
 const Price = () => {
+
+    const [priceMenu , setPriceMenu] = useState('Gas Refill')
 
   return (
 
     <>
 
-        <section id='pricing-cart' className='min-h-screen flex justify-center py-12 px-6'>
+        <section id='pricing-cart' className='min-h-[80%] md:min-h-screen md:grid grid-cols-1 flex flex-col justify-center py-3 md:py-12 px-6'>
 
-            <div className='text-center flex flex-col justify-self-center items-center gap-2.5'>
+            <div className='text-center flex flex-col justify-items-center items-center gap-4'>
 
-                <div className='flex justify-center items-center w-auto text-base bg-gray-200 py-1 px-3 rounded-full'>
+                <p className='py-1 px-3 bg-gray-200 rounded-full text-gray-700'> &#36; Pricing</p>
 
-                    <img src={asstets.payment}/>
+                <h1 className='font-bold text-4xl'> <span className='text-[#0D6EFD]'>GasByGas</span> Pricing Solutions</h1>
 
-                    <p>Price</p>
+                <p className='text-[14.5px] text-gray-500'>No hidden fees, just simple and affordable plans tailored to your needs</p>
 
-                </div>
+                <div className='flex gap-4 bg-gray-100 py-1 px-1.5 rounded-full'>
 
-                <h1>Tailored Pricing Solutions</h1>
-
-                <p>No hidden fees, just simple and affordable plans tailored to your needs</p>
-
-                <div>
-
-                    <button>Gas Refill</button>
+                    <button className={priceMenu === 'Gas Refill' ? "bg-white py-0.5 px-2 text-[15px] text-base text-gray-800 rounded-full" : "bg-transparent py-0.5 px-2 text-[15px] text-base text-gray-800"} onClick={() => setPriceMenu('Gas Refill')}>Gas Refill</button>
                     
-                    <button>Buy Cylinders</button>
+                    <button className={priceMenu === 'Buy Gas' ? 'bg-white py-0.5 px-2 text-[15px] text-base text-gray-800 rounded-full' : 'bg-transparent py-0.5 px-2 text-[15px] text-base text-gray-800'} onClick={() => setPriceMenu('Buy Gas')}>Buy Gas</button>
+
+                </div>
+
+                <div className='grid grid-cols-1 w-full gap-10 mx-auto mt-4 md:grid-cols-3 '>
+
+                    {/* =========================================== Small Gas ============================================== */}
+
+                    <div  className='bg-white py-6  max-w-96 border rounded-md'>
+
+                        <div>
+
+                            <p>Small Gas</p>
+                            <h1>LKR 820.94</h1>
+
+                        </div>
+
+                    </div>
+
+                    <div className='bg-white py-6  max-w-96 border rounded-md'>
+
+                        <div>
+
+                            <p>Medium Gas</p>
+                            <h1>LKR 800</h1>
+
+                        </div>
+
+                    </div>
+
+                    <div className='bg-white py-6  max-w-96 border rounded-md'>
+
+                        <div>
+
+                            <p>Large Gas</p>
+                            <h1>LKR 3922.24</h1>
+
+                        </div>
+                        
+                    </div>
 
                 </div>
 
             </div>
 
-            <div>
 
-                {/* =========================================== Small Gas ============================================== */}
-
-                <div>
-
-                    <h2></h2>
-                </div>
-            </div>
 
         </section>
 
