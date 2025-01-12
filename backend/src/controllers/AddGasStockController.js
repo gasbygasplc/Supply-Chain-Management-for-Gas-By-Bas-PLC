bf5f2cea864d29a692e4144f0ec3970951d9a24e
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-12 21:15:42 +0530

    Enhance addGasStock controller to validate input, handle image uploads, and update existing stock

--- backend/src/controllers/AddGasStockController.js
+++ backend/src/controllers/AddGasStockController.js
@@ -1,4 +1,5 @@
-
+import {v2 as cloudinary} from 'cloudinary'
+import gasStockmodel from '../models/GasStockModels.js';
 
 const addStock = async(req, res) => {
 
@@ -9,14 +10,78 @@ const addStock = async(req, res) => {
 
         const imageFile = req.file;
 
-        console.log({type , weightKG , price , totalStock , stockHistroy}, imageFile)
+        if(!type || !weightKG || !price || !totalStock || !stockHistroy )
+        {
 
+            return res.json({success:false , message : "Missing Information"})
+
+        }
+
+        if(totalStock < 0)
+        {
+            
+            return res.json({success: false , message: "Please Enter valid Stock Amount"});
+
+        }
+
+        const existingStockType = await gasStockmodel.findOne({type});
+
+        if(existingStockType)
+        {
+            existingStockType.totalStock += Number(totalStock);
+
+            const newStockHistory = JSON.parse(stockHistroy);
+
+            existingStockType.stockHistroy.push(...newStockHistory);
+
+            await existingStockType.save();
+
+            res.json({success:true , message:`Gas stock updated successfully.`})
+        }
+        else
+        {
+            try 
+            {
+
+                const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type:"image"});
+
+                const imageURL = imageUpload.secure_url;
+
+                const gasStockData = {
+
+                    type,
+                    weightKG,
+                    price,
+                    image : imageURL,
+                    totalStock,
+                    stockHistroy : JSON.parse(stockHistroy)
+
+                }
+
+                const gasStock = new gasStockmodel(gasStockData);
+
+                await gasStock.save();
+
+                res.json({success:true , message: "Gas Stock Added"});
+
+                
+            } catch (error) 
+            {
+
+                console.log(error);
+
+                res.json({success:false , message: error.message})
+                
+            }
+            
+        }
 
         
     } catch (error) 
     {
 
-
+        console.log(error);
+        res.json({success:false , message: error.message})
         
     }
 
