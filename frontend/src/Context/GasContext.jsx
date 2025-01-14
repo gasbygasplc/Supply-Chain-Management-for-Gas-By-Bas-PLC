b553b3f0c0f9e03388439e64fd523a61f9b3b8c7
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-14 22:13:55 +0530

    Add: Integrate react-toastify for error notifications and update GasContext for gas selection management

--- frontend/src/Context/GasContext.jsx
+++ frontend/src/Context/GasContext.jsx
@@ -1,10 +1,14 @@
 import { createContext, useState } from "react";
+import axios from 'axios';
+import { toast } from "react-toastify";
 
-export const gasContext = createContext();
+export const GasContext = createContext();
 
 const GasContectProvider = (props) => {
 
     const [gasDetails , setGasDetails] = useState([]);
+
+    const [selectedGasDetail , setSelectedGasDetail] = useState(null)
 
     const backendURL = import.meta.env.VITE_BACKEND_URL;
 
@@ -16,28 +20,57 @@ const GasContectProvider = (props) => {
         try 
         {
 
+            const {gasData} = await axios.get(backendURL + '/api/gas/gas-data');
 
+            if(gasData.success)
+            {
+                setGasDetails(gasData.gasData);
+
+                setSelectedGasDetail(gasData.gasData[0]);
+
+                console.log(gasData.gasData);
+            }
+            else
+            {
+                toast.error(gasData.message)
+            }
             
         } catch (error) 
         {
 
-            
+            toast.error(error.message)
             
         }
     }
 
+    //============================================= Handle Gas selection code ===========================================
+
+    const handleGasSelection = gasId => {
+
+        const selected = gasDetails.find((gas) => gas._id === gasId);
+
+        setSelectedGasDetail(selected);
+
+    }
+
+
+
     const value = {
 
-
+        getGasDetails,
+        gasDetails,
+        setGasDetails,
+        setSelectedGasDetail,
+        selectedGasDetail
     }
 
     return(
 
-        <gasContext.Provider value={value}>
+        <GasContext.Provider value={value}>
 
             {props.children}
 
-        </gasContext.Provider>
+        </GasContext.Provider>
     )
 }
 
