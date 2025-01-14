c690f929ee2f8c467ae1214d91630c718a9a3dd6
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-14 15:39:39 +0530

    Refactor Contact Us component layout and enhance content structure

--- frontend/src/Components/ContactUs.jsx
+++ frontend/src/Components/ContactUs.jsx
@@ -5,68 +5,84 @@ const ContactUs = () => {
 
   return (
 
-    <section id='contact-us' className='py-3 md:py-8'>
+    <section id='contact-us' className='py-3 md:py-5'>
 
         <div className='text-center flex flex-col gap-1 justify-center items-center'>
 
             <p className='py-[10px] px-[18px] bg-gray-200 rounded-full text-gray-700'><span className='text-lg'>&#9743; </span> Contact Us</p>
 
-            <h1 className='font-bold py-4 text-4xl'> Get in touch <span className='text-primary'> with our team.</span></h1>
+            <h1 className='font-bold py-2 text-4xl'> Get in touch <span className='text-primary'> with our team.</span></h1>
 
 
         </div>
 
-        <div className='flex flex-col gap-2 items-center'>
+        <div className='flex flex-col w-full gap-2 items-center'>
 
             <img className='w-[60%] bg-white border border-primary rounded mt-4' src={asstets.contact_us} alt="" />
 
-            <div>
+            <div className='flex flex-col w-full md:flex-row gap-4 mt-3 text-sm'>
 
-                <div>
+                <div className='w-[80%] gap-3 p-3 rounded-md cursor-pointer flex flex-col justify-center items-start white border '>
 
-                    <img src="" alt="" />
+                    <div className='p-1 bg-white border rounded-md'>
 
-                    <h6></h6>
+                        <img className='w-6' src={asstets.chat_to_purchase} alt="" />
 
-                    <p></p>
+                    </div>
 
-                    <button></button>
+                    <h6 className='text-sm font-semibold'>Chat to Purchase</h6>
 
-                </div>
+                    <p className='text-gray-500'>Speak to our friendly team.</p>
 
-                <div>
-
-                    <img src="" alt="" />
-
-                    <h6></h6>
-
-                    <p></p>
-
-                    <button></button>
+                    <button>sales@gasbygas.com</button>
 
                 </div>
 
-                <div>
+                <div className='w-[80%] items-start gap-3 p-3 rounded-md cursor-pointer flex flex-col   bg-white border text-sm'>
 
-                    <img src="" alt="" />
+                    <div className='p-1 bg-white border rounded-md'>
 
-                    <h6></h6>
+                        <img className='w-6' src={asstets.chat_to_support} alt="" />
 
-                    <p></p>
+                    </div>
 
-                    <button></button>
+                    <h6 className='text-sm font-semibold'>Chat to support</h6>
+
+                    <p className='text-gray-500'>we are here to help.</p>
+
+                    <button>support@gasbygas.com</button>
 
                 </div>
 
-                <div>
+                <div className='w-[80%] items-start gap-3 p-3 rounded-md cursor-pointer flex flex-col   bg-white border text-sm'>
 
-                    <img src="" alt="" />
+                    <div className='p-1 bg-white border rounded-md'>
 
-                    <h6></h6>
+                        <img className='w-6' src={asstets.location} alt="" />
 
-                    <p></p>
+                    </div>
 
-                    <button></button>
+                    <h6 className='text-sm font-semibold'>Visit Us</h6>
+
+                    <p className='text-gray-500'>Visit your near by Outlet</p>
+
+                    <button>call to near outlet</button>
+
+                </div>
+
+                <div className='w-[80%] items-start gap-3 p-3 rounded-md cursor-pointer flex flex-col   bg-white border text-sm'>
+
+                    <div className='p-1 bg-white border rounded-md'>
+
+                        <img className='w-6' src={asstets.call} alt="" />
+
+                    </div>
+
+                    <h6 className='text-sm font-semibold'>Call Us</h6>
+
+                    <p className='text-gray-500'>Mon - Sun from 8am to 7pm</p>
+
+                    <button>0094-76 413 2578</button>
 
                 </div>
 
