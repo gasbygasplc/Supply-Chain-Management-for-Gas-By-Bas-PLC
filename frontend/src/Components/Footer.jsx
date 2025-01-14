5c07111711352f515a29695e9354750623dd7930
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-14 18:25:18 +0530

    Add Footer component with initial structure and layout

---
+++ frontend/src/Components/Footer.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { asstets } from '../assets/Assets'
+
+const Footer = () => {
+
+  return (
+
+    <footer>
+
+        {/* ========================================= Left Side ================================================= */}
+
+        <div>
+
+          <img src={asstets.logo} alt="" />
+
+          <p></p>
+
+        </div>
+
+        {/* ========================================== Center Part ============================================= */}
+
+        <div>
+
+          <p></p>
+
+          <ul>
+
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+
+          </ul>
+
+        </div>
+
+        {/* ============================================ Right SIde =========================================== */}
+
+        <div>
+
+          <p></p>
+
+          <ul>
+
+            <li></li>
+            <li></li>
+
+          </ul>
+
+        </div>
+
+    </footer>
+
+  )
+
+}
+
+export default Footer
