5264926a2d199ca91632427dfda2d78aa1b0d670
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-14 18:36:11 +0530

    Enhance Footer component with improved copyright section and text styling

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
 
   return (
 
-    <footer className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+    <footer className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-20 text-sm'>
 
       {/* ========================================= Left Side ================================================= */}
 
@@ -13,17 +13,17 @@ const Footer = () => {
 
           <img className='mb-5 w-30' src={asstets.logo} alt="" />
 
-          <p>Gas by Gas is your trusted partner for reliable, affordable, and eco-friendly gas solutions. We’re committed to delivering exceptional service and top-quality products to meet your energy needs.</p>
+          <p className='w-full md:w-2/3 text-gray-600 leading-6'>Gas by Gas is your trusted partner for reliable, affordable, and eco-friendly gas solutions. We’re committed to delivering exceptional service and top-quality products to meet your energy needs.</p>
 
         </div>
 
         {/* ========================================== Center Part ============================================= */}
 
-        <div>
+        <div >
 
-          <p>Gas By Gas</p>
+          <p className='text-xl font-medium mb-5 text-primary'>Gas By Gas</p>
 
-          <ul>
+          <ul className='flex flex-col gap-2 text-gray-600'>
 
             <li>Home</li>
             <li>price</li>
@@ -47,6 +47,16 @@ const Footer = () => {
             <li>gasbygasplc@gmail.com</li>
 
           </ul>
+
+        </div>
+
+        {/* ============================================== Footer Copyrights ==================================== */}
+
+        <div>
+
+          <hr />
+
+          <p>Copyright 2025@ GasByGas - All Rights Reserved</p>
 
         </div>
 
