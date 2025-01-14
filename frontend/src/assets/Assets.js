79ec50f71c97f3e20303762ac1ceb87ec2426e1f
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-14 15:13:24 +0530

    Add additional SVG assets for chat support and location features

--- frontend/src/assets/Assets.js
+++ frontend/src/assets/Assets.js
@@ -4,6 +4,10 @@ import bucket_icon from './add_shopping_cart.svg'
 import payment from './payments.svg';
 import about_us from './about.jpeg';
 import contact_us from './contact-us.svg'
+import chat_to_purchase from './chat_to_support.svg';
+import chat_to_support from './chat_to_purchase.svg';
+import location from './Visit_us.svg';
+import call from './call.svg'
 
 export const asstets = {
 
@@ -12,6 +16,10 @@ export const asstets = {
     header_image,
     payment,
     about_us,
-    contact_us
+    contact_us,
+    chat_to_purchase,
+    chat_to_support,
+    location,
+    call
 
 }
