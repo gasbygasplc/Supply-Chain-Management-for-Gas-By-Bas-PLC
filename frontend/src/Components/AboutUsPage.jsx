4e70893ed820195a83858c4a9253a4beb9c3e804
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-14 12:29:12 +0530

    Update AboutUsPage styling and replace cover image for improved presentation

--- frontend/src/Components/AboutUsPage.jsx
+++ frontend/src/Components/AboutUsPage.jsx
@@ -5,33 +5,37 @@ const AboutUsPage = () => {
 
   return (
 
-    <section id='about-us' className='min-h-[80%] gap-0 md:min-h-screen md:grid grid-cols-1 flex flex-col justify-center py-3 md:py-8 px-6'>
+    <section id='about-us' className='py-3 md:py-4'>
 
-        {/* ============================================ Our Company ============================================== */}
+      {/* ============================================ Our Company ============================================== */}
 
-        <div className='text-center p-0 m-0 h-auto'>
+      <div className='text-center'>
 
-            <h1 className='font-bold text-4xl'><span className='text-[#0D6EFD]'>Our</span> Company</h1>
+        <h1 className='font-bold py-4 text-4xl'><span className='text-[#0D6EFD]'>Our</span> Company</h1>
 
-            <p className='text-sm text-gray-500 py-0'>Fueling Your Home, Business, and Beyond – Reliable Gas Cylinders Delivered and Refilled with Care. <br/> Let Us Power Your Everyday Moments!</p>
+        <p className='text-sm text-gray-500 py-0'>Fueling Your Home, Business, and Beyond – Reliable Gas Cylinders Delivered and Refilled with Care. <br/> Let Us Power Your Everyday Moments!</p>
+
+      </div>
+
+      {/* =============================================== Image ================================================= */}
+
+      <div className='flex flex-col md:flex-row gap-12 my-10'>
+
+        <img className='w-full md:max-w-[360px] rounded-md' src={asstets.about_us} alt="" />
+
+        <div className=' flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-500'>
+
+          <p>Welcome to Gas By Gas, your reliable partner for all your gas cylinder refill and purchase needs. At GasEase, we understand the importance of a steady and safe supply of cooking and industrial gas for your home or business. Our platform is designed to make ordering gas cylinders and scheduling refills seamless, ensuring convenience and safety every step of the way. With a commitment to quality and timely delivery, GasEase is here to simplify your gas management and keep your daily operations running smoothly</p>
+
+          <p>GasEase is committed to excellence in gas supply and delivery services. We continually enhance our platform, incorporating the latest technologies to provide a seamless user experience and exceptional service. Whether you’re scheduling your first refill or managing recurring orders, GasEase is here to support you with reliable solutions every step of the way</p>
+
+          <b className='text-primary text-lg'>Our Vision</b>
+
+          <p>To revolutionize the way individuals and businesses access and manage their gas supply by providing a seamless, efficient, and eco-friendly platform that ensures safety, reliability, and convenience. We aim to be the most trusted name in gas cylinder refill and delivery services, fostering sustainability and innovation in every aspect of our operations.</p>
 
         </div>
-
-        {/* =============================================== Image ================================================= */}
-
-        <div className='min-h-[10vw] bg-center mt-0 sm:min-h-[10vw] bg-no-repeat bg-contain' style={{backgroundImage:`url(${asstets.about_us})`}}>
         
-
-
-        </div>
-
-        {/* ============================================ Work with Clients ========================================= */}
-
-        <div>
-
-
-
-        </div>
+      </div>
 
     </section>
 
--- frontend/src/assets/Assets.js
+++ frontend/src/assets/Assets.js
@@ -2,7 +2,7 @@ import logo from './Logo.svg'
 import header_image from './Header_Image.svg'
 import bucket_icon from './add_shopping_cart.svg'
 import payment from './payments.svg';
-import about_us from './About-us-page-cover.svg'
+import about_us from './about.jpeg'
 
 export const asstets = {
 
