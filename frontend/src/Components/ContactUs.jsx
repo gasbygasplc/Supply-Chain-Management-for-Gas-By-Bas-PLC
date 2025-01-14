13b99d9f5224299a48ee9decf72ca746152626dc
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-14 14:56:32 +0530

    Add Contact Us section with styling and import contact icon

--- frontend/src/Components/ContactUs.jsx
+++ frontend/src/Components/ContactUs.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import { asstets } from '../assets/Assets'
 
 const ContactUs = () => {
 
   return (
 
-    <section id='contact-us'>
+    <section id='contact-us' className='py-3 md:py-8'>
 
+        <div className='text-center flex flex-col gap-3'>
+
+            <p className='py-[10px] px-[18px] bg-gray-200 rounded-full text-gray-700'>Contact Us</p>
+
+            <h1>Get in touch with our team.</h1>
+
+            <p>We have the outlets and know-how to help you scale 10x faster</p>
+
+        </div>
+
+        <div>
+
+
+
+        </div>
 
     </section>
 
--- frontend/src/assets/Assets.js
+++ frontend/src/assets/Assets.js
@@ -2,7 +2,8 @@ import logo from './Logo.svg'
 import header_image from './Header_Image.svg'
 import bucket_icon from './add_shopping_cart.svg'
 import payment from './payments.svg';
-import about_us from './about.jpeg'
+import about_us from './about.jpeg';
+import contact_us from './contact-us.svg'
 
 export const asstets = {
 
@@ -10,6 +11,7 @@ export const asstets = {
     bucket_icon,
     header_image,
     payment,
-    about_us
+    about_us,
+    contact_us
 
 }
