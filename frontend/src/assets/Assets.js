66d9602c9f52fdbe2f928f8aaebf74dae40017e2
mathumithasrikanthan <mathumithasrikanthan@gmail.com>
2025-01-14 19:46:25 +0530

    Add new icons for add and remove actions in Assets.js

--- frontend/src/assets/Assets.js
+++ frontend/src/assets/Assets.js
@@ -4,7 +4,9 @@ import bucket_icon from './add_shopping_cart.svg'
 import payment from './payments.svg'
 import small_gas from './small.png';
 import medium_gas from './medium.png';
-import large_gas from './large.png'
+import large_gas from './large.png';
+import add_icon from './add_icon_green.png';
+import remove_icon from './remove_icon_red.png';
 
 export const asstets = {
 
@@ -14,6 +16,8 @@ export const asstets = {
     payment,
     small_gas,
     medium_gas,
-    large_gas
+    large_gas,
+    add_icon,
+    remove_icon
 
 }
