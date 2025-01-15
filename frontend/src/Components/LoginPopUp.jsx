c4fd662f5b32116d245933474d5d835c628607e4
mathumithasrikanthan <mathumithasrikanthan@gmail.com>
2025-01-15 20:43:10 +0530

    Enhance LoginPopUp component with additional input fields and user role selection

--- frontend/src/Components/LoginPopUp.jsx
+++ frontend/src/Components/LoginPopUp.jsx
@@ -20,9 +20,32 @@ const LoginPopUp = () => {
 
             <div>
 
-                
+                <input type="text" />
+                <input type="email" />
+                <input type="password" />
+                <input type="text" />
+                <select defaultValue={'User'}>
+
+                    <option value="User">User</option>
+                    <option value="Organization">Organization</option>
+
+                </select>
 
             </div>
+
+            <button>Sign In</button>
+
+            <div>
+
+                <input type="checkbox" />
+
+                <p>By Continuing, i agree to the terms of use & Privacy Policy</p>
+
+            </div>
+
+            <p>Create a new Account? <span>Click Here</span></p>
+
+
         </form>
 
     </div>
