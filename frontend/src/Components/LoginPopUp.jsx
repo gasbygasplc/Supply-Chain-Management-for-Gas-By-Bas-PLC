30606d64ff03eca5e7b1ec6b639793882637272e
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-15 22:36:13 +0530

    Pass setShowSignIn prop to LoginPopUp component to manage visibility state

--- frontend/src/Components/LoginPopUp.jsx
+++ frontend/src/Components/LoginPopUp.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { asstets } from '../assets/Assets'
 
-const LoginPopUp = () => {
+const LoginPopUp = ({setShowSignIn}) => {
 
     const [currentState , setCurrentState] = useState('Sign In');
 
@@ -12,12 +12,11 @@ const LoginPopUp = () => {
         <form className='bg-white flex flex-col gap-6 p-6 rounded-lg w-[95%] sm:w-[330px] md:w-[24vw] fadeIn'>
 
 
-
             <div className='flex justify-between items-center text-black'>
 
                 <h2 className='text-lg font-semibold'>{currentState}</h2>
 
-                <img className='w-4 cursor-pointer' src={asstets.cross_icon} alt='closer' />
+                <img onClick={() => setShowSignIn(previous => previous ? false : true)} className='w-4 cursor-pointer' src={asstets.cross_icon} alt='closer' />
 
             </div>
 
