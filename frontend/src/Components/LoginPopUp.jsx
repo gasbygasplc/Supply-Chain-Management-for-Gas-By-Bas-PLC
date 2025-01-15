372a3febbbde5fb9f7c52ecd85dadae008e24454
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-15 22:18:09 +0530

    Refactor LoginPopUp component to manage state for dynamic form fields and enhance styling for better user interaction

--- frontend/src/Components/LoginPopUp.jsx
+++ frontend/src/Components/LoginPopUp.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { asstets } from '../assets/Assets'
 
 const LoginPopUp = () => {
+
+    const [currentState , setCurrentState] = useState('Sign In');
 
   return (
 
     <div className='absolute inset-0 z-10 bg-black bg-opacity-70 grid place-items-center'>
         
-        <form className='bg-white flex flex-col gap-6 p-6 rounded-lg w-[90%] sm:w-[330px] md:w-[24vw] fadeIn'>
+        <form className='bg-white flex flex-col gap-6 p-6 rounded-lg w-[95%] sm:w-[330px] md:w-[24vw] fadeIn'>
 
 
 
             <div className='flex justify-between items-center text-black'>
 
-                <h2 className='text-lg font-semibold'>Sign In</h2>
+                <h2 className='text-lg font-semibold'>{currentState}</h2>
 
                 <img className='w-4 cursor-pointer' src={asstets.cross_icon} alt='closer' />
 
@@ -21,22 +23,27 @@ const LoginPopUp = () => {
 
             <div className='flex flex-col gap-4'>
 
-                <input type="text" placeholder='Gas by Gas' className='outline-none  border border-primary p-2 rounded-md'/>
                 <input type="email" placeholder='Gasbygas@gmail.com' className='outline-none  border border-primary p-2 rounded-md'/>
                 <input type="password" placeholder='**********' className='outline-none  border border-primary p-2 rounded-md'/>
-                <input type="text" placeholder='0094771234567' className='outline-none  border border-primary p-2 rounded-md'/>
-                <select defaultValue={'User'} className='outline-none  border border-primary p-2 rounded-md'>
 
-                    <option value="User">User</option>
-                    <option value="Organization">Organization</option>
+                {currentState === 'Sign In' ? <></> : <>
+                    
+                    <input type="text" placeholder='Gas by Gas' className='outline-none  border border-primary p-2 rounded-md'/>
+                    <input type="text" placeholder='0094771234567' className='outline-none  border border-primary p-2 rounded-md'/>
+                    <select defaultValue={'User'} className='outline-none  border border-primary p-2 rounded-md'>
 
-                </select>
+                        <option value="User">User</option>
+                        <option value="Organization">Organization</option>
+
+                    </select>
+
+                </>}
 
             </div>
 
             <button className='bg-primary text-white py-2 rounded-md text-sm font-medium cursor-pointer'>Sign In</button>
 
-            <div className='flex items-center gap-2 mt-4'>
+            <div className='flex items-center gap-2'>
 
                 <input type="checkbox" />
 
@@ -44,7 +51,7 @@ const LoginPopUp = () => {
 
             </div>
 
-            <p className='text-sm'>Create a new Account? <span className='text-sm font-semibold cursor-pointer'>Click Here</span></p>
+            <p className='text-sm'>Create a new Account? <span className='text-sm font-semibold cursor-pointer text-primary'>Click Here</span></p>
 
 
         </form>
