8f135fcd0ad99f7d570e2ff04ed66849d8cc82f8
mathumithasrikanthan <mathumithasrikanthan@gmail.com>
2025-01-16 19:58:32 +0530

    Add unique NIC field to User model and enhance registration validation

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -8,10 +8,12 @@ const UserSchema = new mongoose.Schema(
 
     email: { type: String, unique: true, required: true },
 
+    nic: { type: String, unique: true, required: true },
+
     password: { type: String, required: true },
 
     phone: { type: String, unique: true, required: true },
-    
+
     role: { type: String, enum: ['User', 'Organization'], default: 'User' },
     
   },
