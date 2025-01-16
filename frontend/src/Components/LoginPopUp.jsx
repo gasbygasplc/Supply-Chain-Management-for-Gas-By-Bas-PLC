f2d11a1226c75aa09cd4098614c2b653243aac67
mathumithasrikanthan <mathumithasrikanthan@gmail.com>
2025-01-16 21:22:07 +0530

    Enhance LoginPopUp component with form submission handling and input normalization

--- frontend/src/Components/LoginPopUp.jsx
+++ frontend/src/Components/LoginPopUp.jsx
@@ -1,9 +1,14 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { asstets } from '../assets/Assets'
+import { GasContext } from '../Context/GasContext';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const LoginPopUp = ({setShowSignIn}) => {
 
     const [currentState , setCurrentState] = useState('Sign In');
+
+    const {backendURL} = useContext(GasContext);
 
     const [data , setData] = useState({
 
@@ -35,10 +40,59 @@ const LoginPopUp = ({setShowSignIn}) => {
 
     //onsubmit handler function
 
-    const onSubmitHandler = (e) => {
+    const onSubmitHandler = async (e) => {
 
         e.preventDefault();
 
+        const normalizedPhone  = data.phone.startsWith('94') ? data.phone : `94${data.phone.replace(/^0/, '')}`;
+
+        const payLoad = {...data , phone : normalizedPhone};
+
+        let localHost = 'http://localhost:4000';
+
+        if(currentState === 'Sign In')
+        {
+
+            localHost += '/api/auth/login';
+        }
+        else
+        {
+
+            localHost += '/api/auth/register';
+
+        }
+
+        try
+        {
+
+            const response  = await axios.post(localHost , payLoad);
+
+            if(response.status === 201)
+            {
+
+                toast.success(response.message);
+                data.name = '';
+                data.phone = '';
+                data.nic = '';
+                data.role = '';
+                data.email = '';    
+                data.password = '';
+
+            }
+            else
+            {
+
+                toast.error(response.message);
+            }
+
+        }
+        catch(error)
+        {
+
+            console.error("Error submitting form:", error);
+            toast.error("Error submitting form");
+
+        }
         
     }
 
@@ -46,7 +100,7 @@ const LoginPopUp = ({setShowSignIn}) => {
 
     <div className='absolute inset-0 z-10 bg-black bg-opacity-70 grid place-items-center'>
         
-        <form className='bg-white flex flex-col gap-6 p-6 rounded-lg w-[95%] sm:w-[330px] md:w-[24vw] fadeIn'>
+        <form onSubmit={onSubmitHandler} className='bg-white flex flex-col gap-6 p-6 rounded-lg w-[95%] sm:w-[330px] md:w-[24vw] fadeIn'>
 
 
             <div className='flex justify-between items-center text-black'>
@@ -61,21 +115,21 @@ const LoginPopUp = ({setShowSignIn}) => {
 
                 {currentState === 'Sign In' ? <></> : <>
                     
-                <input type="text" placeholder='Mathumitha' className='outline-none  border border-primary p-2 rounded-md'/>
-                <input type="text" placeholder='0094771234567' className='outline-none  border border-primary p-2 rounded-md'/>
-                <input type="text" placeholder='200118706543V' className='outline-none  border border-primary p-2 rounded-md'/>
-                <select defaultValue={'User'} className='outline-none  border border-primary p-2 rounded-md'>
+                <input onChange={onchangeHandler} value={data.name} name='name' type="text" placeholder='Mathumitha' className='outline-none  border border-primary p-2 rounded-md'/>
+                <input onChange={onchangeHandler} value={data.phone} name='phone' type="text" placeholder='0094771234567' className='outline-none  border border-primary p-2 rounded-md'/>
+                <input onChange={onchangeHandler} value={data.nic} name='nic' type="text" placeholder='200118706543V' className='outline-none  border border-primary p-2 rounded-md'/>
+                <select onChange={onchangeHandler} value={data.role} name='role' className='outline-none  border border-primary p-2 rounded-md'>
 
                     <option value="User">User</option>
                     <option value="Organization">Organization</option>
 
                 </select> </>}
-                <input type="email" placeholder='Gasbygas@gmail.com' className='outline-none  border border-primary p-2 rounded-md'/>
-                <input type="password" placeholder='**********' className='outline-none  border border-primary p-2 rounded-md'/>
+                <input onChange={onchangeHandler} value={data.email} name='email' type="email" placeholder='Gasbygas@gmail.com' className='outline-none  border border-primary p-2 rounded-md'/>
+                <input onChange={onchangeHandler} value={data.password} name='password' type="password" placeholder='**********' className='outline-none  border border-primary p-2 rounded-md'/>
 
             </div>
 
-            <button className='bg-primary text-white py-[10px] rounded-md text-sm font-medium cursor-pointer'>{currentState}</button>
+            <button type='submit' className='bg-primary text-white py-[10px] rounded-md text-sm font-medium cursor-pointer'>{currentState}</button>
 
             <div className='flex items-center gap-2'>
 
