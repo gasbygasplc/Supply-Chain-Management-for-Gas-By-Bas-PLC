5ced82e68aac0f02959d56c3a003de00f2761c95
NayagamRenuga <nayagamrenuga@gmail.com>
2025-01-16 22:27:14 +0530

    modofied App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,27 @@
-
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './Pages/Home'
-import Navbar from './Components/Navbar'
-import Price from './Components/Price'
-import Footer from './Components/Footer'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import Navbar from './Components/Navbar';
+import Price from './Components/Price';
+import Footer from './Components/Footer';
+import DeliveryStatus from './Components/DeliveryStatus'; 
 
 const App = () => {
-
   return (
-
-    <div className='mx-4 sm:mx-[10%]'>
-
-      <Navbar/>
+    <div className="mx-4 sm:mx-[10%]">
+      <Navbar />
 
       <Routes>
+        {/* Home Page */}
+        <Route path="/" element={<Home />} />
 
-        <Route path='/' element ={<Home/>}/>
-
+        {/* Delivery Status Page */}
+        <Route path="/delivery-status/:deliveryId" element={<DeliveryStatus />} />
       </Routes>
 
-      <Footer/>
-
+      <Footer />
     </div>
+  );
+};
 
-  )
-
-}
-
-export default App
+export default App;
