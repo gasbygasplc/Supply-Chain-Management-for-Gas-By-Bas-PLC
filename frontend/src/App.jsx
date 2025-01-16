6c965b610c749ce8ec8029352dcecc3ff6574b29
NayagamRenuga <nayagamrenuga@gmail.com>
2025-01-16 22:30:32 +0530

    modofied App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,21 +7,28 @@ import Footer from './Components/Footer';
 import DeliveryStatus from './Components/DeliveryStatus'; 
 
 const App = () => {
+
   return (
+
     <div className="mx-4 sm:mx-[10%]">
+
       <Navbar />
 
       <Routes>
-        {/* Home Page */}
+        
         <Route path="/" element={<Home />} />
 
-        {/* Delivery Status Page */}
+        
         <Route path="/delivery-status/:deliveryId" element={<DeliveryStatus />} />
+
       </Routes>
 
       <Footer />
+
     </div>
+
   );
+  
 };
 
 export default App;
