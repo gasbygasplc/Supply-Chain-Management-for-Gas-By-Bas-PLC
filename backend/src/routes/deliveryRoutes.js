0c6599afdd08394636d95888fa9631cac5d3a7f3
NayagamRenuga <nayagamrenuga@gmail.com>
2025-01-18 10:05:02 +0530

    modified deliveryRoutes.js

--- backend/src/routes/deliveryRoutes.js
+++ backend/src/routes/deliveryRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
+
 import { scheduleDelivery, updateDeliveryStatus } from '../controllers/deliveryController.js';
 
 const router = express.Router();
 
 router.post('/schedule', scheduleDelivery);
+
 router.put('/status/:deliveryId', updateDeliveryStatus);
 
 export default router;
