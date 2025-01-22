5a1bc14c6e26137fcccd78165519cef07eb6e185
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-22 19:53:25 +0530

    Add OutletContext and OutletContextProvider for managing outlet state

---
+++ frontend/src/Context/OutletContext.jsx
@@ -0,0 +1,23 @@
+import { createContext } from "react";
+
+export const OutletContext = createContext();
+
+const OutletContextProvider = (props) => {
+
+    const value = {
+
+
+    }
+
+    return (
+
+        <OutletContext.Provider value={value}>
+
+            {props.children}
+
+        </OutletContext.Provider>
+    )
+
+}
+
+export default OutletContextProvider;
