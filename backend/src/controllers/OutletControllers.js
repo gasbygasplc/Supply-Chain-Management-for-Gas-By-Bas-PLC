c1b54d5df89b0217a4825a41c9989e15fa34fba0
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-23 13:12:30 +0530

    Update getOutletLocation to retrieve city instead of location

--- backend/src/controllers/OutletControllers.js
+++ backend/src/controllers/OutletControllers.js
@@ -57,16 +57,16 @@ const getOutletLocation = async(req , res) => {
     try 
     {
 
-        const location = await outletModel.find({} , 'Location _id');
+        const city = await outletModel.find({} , 'city _id');
 
-        if(!location.length)
+        if(!city.length)
         {
 
             return res.status(400).json({success:false , message:"No Location Found"});
 
         }
 
-        res.status(200).json({success:true , message: 'Outlet locations and IDs retrieved successfully.' , location});
+        res.status(200).json({success:true , message: 'Outlet locations and IDs retrieved successfully.' , city});
 
 
         
