263c9d4786f5fdb02c6cc4aef5b51c002de411ef
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-25 17:33:16 +0530

    Remove unused Dashboard import from App component

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,7 +5,6 @@ import { AdminContext } from './Context/AdminContext';
 import Navbar from './Components/Navbar';
 import Sidebar from './Components/Sidebar';
 import { Route, Routes } from 'react-router-dom';
-import Dashboard from './Pages/Admin/Dashboard';
 import AddOutlet from './Pages/Admin/AddOutlet';
 import AddOutletManager from './Pages/Admin/AddOutletManager';
 import OutletStockRequest from './Pages/Admin/OutletStockRequest';
