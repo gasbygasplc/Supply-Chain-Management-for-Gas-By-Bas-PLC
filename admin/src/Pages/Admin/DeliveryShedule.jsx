d8a0eed0ba02b80ec3e768649a1a55f53cfc6aa5
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-25 14:34:46 +0530

    [Feature] Enhance Delivery Schedule form with outlet details and gas type selection

--- admin/src/Pages/Admin/DeliveryShedule.jsx
+++ admin/src/Pages/Admin/DeliveryShedule.jsx
@@ -3,15 +3,58 @@ import React from 'react'
 const DeliveryShedule = () => {
   return (
     
-    <section className='w-full'>
+    <div className='w-full sm:w-[40%] bg-white my-8 border mx-8 rounded-md'>
 
-        <form className='w-[50%] bg-white '>
+      <form className='flex flex-col gap-4 px-8 py-8 text-gray-700 text-base'>
 
-            
-            
-        </form>
+        <select className=' -ml-4 -mr-4 px-4 py-2  border border-gray-300 rounded-md focus:outline-none focus:border-primary'>
 
-    </section>
+          <option value="Colombo">Colombo</option>
+
+        </select>
+
+        <div className='bg-white flex flex-col gap-4 border -mx-4 px-4 py-2 rounded-md'>
+
+          <div className='flex justify-between'>
+
+            <label className='font-semibold'>Outlet Name:</label>
+
+            <label>Colombo</label>
+
+          </div>
+
+          <div className='flex justify-between'>
+
+            <label className='font-semibold'>Total Gas Request:</label>
+
+            <label>10</label>
+
+          </div>
+
+          <select name='Type' defaultValue="Select Gas Type" className='px-4 py-2  border border-gray-300 rounded-md focus:outline-none focus:border-primary'>
+
+            <option disabled value="Select Gas Type">Select Gas Type</option>
+
+            <option value="Small">Small</option>
+
+            <option value="Medium">Medium</option>
+
+            <option value="Large">Large</option>
+
+          </select>
+
+          <div>
+
+            <input type="number" placeholder='Quantity' className='w-full px-4 py-2  border border-gray-300 rounded-md focus:outline-none focus:border-primary'/>
+
+          </div>
+
+
+        </div>
+
+      </form>
+
+    </div>
 
   )
 }
