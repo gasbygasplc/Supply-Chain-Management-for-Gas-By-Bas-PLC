7aafbb672d665fad292eec4b7d1503f43964cb87
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-28 18:42:46 +0530

    Refactor StockRequest component layout and update input fields for gas quantities and expected delivery date

--- admin/src/Pages/Outlet/StockRequest.jsx
+++ admin/src/Pages/Outlet/StockRequest.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 const StockRequest = () => {
   return (
 
-    <div className='w-full md:w-[60%] text-gray-700 text-base mt-6 rounded-md py-1 px-6 mx-auto md:mx-[2%] bg-white border'>
+    <div className='w-full md:w-[60%] text-gray-700 text-base mt-6 rounded-md py-6 px-6 mx-auto md:mx-[2%] bg-white border'>
 
-      <h1 className='font-semibold py-2 text-2xl md:text-2xl mb-4'>Delivery Shedule Stock Request</h1>
+      <h1 className='font-semibold text-2xl md:text-2xl mb-4'>Delivery Shedule Stock Request</h1>
 
       <div className='w-full flex flex-col gap-6'>
 
         <div className='flex flex-col md:grid grid-cols-6 md:gap-6'>
 
-          <div className='w-full flex flex-col md:col-span-2 gap-2'>
+          <div className='w-full flex flex-col md:col-span-3 gap-2'>
 
             <label className='block' htmlFor="smallGas">Small Gas</label>
 
@@ -19,7 +19,7 @@ const StockRequest = () => {
 
           </div>
 
-          <div className='w-full flex flex-col md:col-span-2 gap-2'>
+          <div className='w-full flex flex-col md:col-span-3 gap-2'>
 
             <label htmlFor="smallGas">Medium Gas</label>
 
@@ -27,14 +27,6 @@ const StockRequest = () => {
 
           </div>
 
-          <div className='w-full flex flex-col md:col-span-2 gap-2'>
-
-            <label htmlFor="smallGas">Large Gas</label>
-
-            <input className='w-full p-2 border border-gray-300 rounded-md focus:outline-1 focus:outline-primary-700' type="number" placeholder='Small Gas Quantity'/>
-
-          </div>
-
           <div className='w-full flex flex-col md:col-span-3 gap-2'>
 
             <label htmlFor="smallGas">Large Gas</label>
@@ -45,9 +37,9 @@ const StockRequest = () => {
 
           <div className='w-full flex flex-col md:col-span-3 gap-2'>
 
-            <label htmlFor="smallGas">Large Gas</label>
+            <label htmlFor="smallGas">Expected Date</label>
 
-            <input className='w-full p-2 border border-gray-300 rounded-md focus:outline-1 focus:outline-primary-700' type="number" placeholder='Small Gas Quantity'/>
+            <input className='w-full p-2 border border-gray-300 rounded-md focus:outline-1 focus:outline-primary-700' type="date" placeholder='Small Gas Quantity'/>
 
           </div>
 
