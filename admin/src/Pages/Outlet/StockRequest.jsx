c3be1e8880b7bb24ef8436181e6009176654e362
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-28 18:36:47 +0530

    Enhance StockRequest component with delivery schedule form for gas quantities

--- admin/src/Pages/Outlet/StockRequest.jsx
+++ admin/src/Pages/Outlet/StockRequest.jsx
@@ -2,7 +2,60 @@ import React from 'react'
 
 const StockRequest = () => {
   return (
-    <div>StockRequest</div> 
+
+    <div className='w-full md:w-[60%] text-gray-700 text-base mt-6 rounded-md py-1 px-6 mx-auto md:mx-[2%] bg-white border'>
+
+      <h1 className='font-semibold py-2 text-2xl md:text-2xl mb-4'>Delivery Shedule Stock Request</h1>
+
+      <div className='w-full flex flex-col gap-6'>
+
+        <div className='flex flex-col md:grid grid-cols-6 md:gap-6'>
+
+          <div className='w-full flex flex-col md:col-span-2 gap-2'>
+
+            <label className='block' htmlFor="smallGas">Small Gas</label>
+
+            <input className='w-full p-2 border border-gray-300 rounded-md focus:outline-1 focus:outline-primary-700' type="number" placeholder='Small Gas Quantity'/>
+
+          </div>
+
+          <div className='w-full flex flex-col md:col-span-2 gap-2'>
+
+            <label htmlFor="smallGas">Medium Gas</label>
+
+            <input className='w-full p-2 border border-gray-300 rounded-md focus:outline-1 focus:outline-primary-700' type="number" placeholder='Small Gas Quantity'/>
+
+          </div>
+
+          <div className='w-full flex flex-col md:col-span-2 gap-2'>
+
+            <label htmlFor="smallGas">Large Gas</label>
+
+            <input className='w-full p-2 border border-gray-300 rounded-md focus:outline-1 focus:outline-primary-700' type="number" placeholder='Small Gas Quantity'/>
+
+          </div>
+
+          <div className='w-full flex flex-col md:col-span-3 gap-2'>
+
+            <label htmlFor="smallGas">Large Gas</label>
+
+            <input className='w-full p-2 border border-gray-300 rounded-md focus:outline-1 focus:outline-primary-700' type="number" placeholder='Small Gas Quantity'/>
+
+          </div>
+
+          <div className='w-full flex flex-col md:col-span-3 gap-2'>
+
+            <label htmlFor="smallGas">Large Gas</label>
+
+            <input className='w-full p-2 border border-gray-300 rounded-md focus:outline-1 focus:outline-primary-700' type="number" placeholder='Small Gas Quantity'/>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
