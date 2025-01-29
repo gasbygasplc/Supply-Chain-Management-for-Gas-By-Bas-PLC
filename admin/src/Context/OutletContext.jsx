94043a99f3cc8e705e7086bef427a943799e28f2
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-29 21:14:17 +0530

    Add fetchGasReq function to retrieve gas schedule requests and update context state

--- admin/src/Context/OutletContext.jsx
+++ admin/src/Context/OutletContext.jsx
@@ -14,6 +14,8 @@ const OutletContextProvider = (props) => {
 
     const [gasRequest , setGasRequest] = useState([]);
 
+    const [gasSheduleReq , setGasSheduleReq] = useState([]);
+
     //============================================== get Outlet ====================================================
 
     const getOutletName = async() => {
@@ -77,6 +79,37 @@ const OutletContextProvider = (props) => {
 
         }
     };
+
+    //============================================ get Gas Shedule request ===============================================
+
+    const fetchGasReq = async() => {
+
+        try 
+        {
+
+            const response = await axios.get("http://localhost:4000/api/outlet/gas-requests" , {} , {headers:{Otoken}});
+
+            if(response.data.success)
+            {
+                setGasSheduleReq(response.data.request);
+            }
+
+            
+            
+        } catch (error) 
+        {
+            console.error("Error fetching gas requests:", error);
+        }
+
+    }
+
+    useEffect(() => {
+
+        if(Otoken)
+        {
+            fetchGasReq();
+        }
+    } , [Otoken])
     
 
     
@@ -90,6 +123,7 @@ const OutletContextProvider = (props) => {
         getOutletName,
         getGasRequest,
         gasRequest,
+        gasSheduleReq
     }
     
 
