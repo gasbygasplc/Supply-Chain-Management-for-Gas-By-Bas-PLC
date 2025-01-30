0cecf03045384d751ba8eaec7b5e796566642773
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-30 18:42:21 +0530

    Refactor Outlet Dashboard layout for improved readability and organization

--- admin/src/Pages/Outlet/OutletDashboard.jsx
+++ admin/src/Pages/Outlet/OutletDashboard.jsx
@@ -20,43 +20,42 @@ const OutletDashboard = () => {
           <p className='py-[5px] px-[13px] border bg-white text-gray-600 rounded-full'>{currentDate}</p>
         </div>
 
-        <div className='w-full flex'>
+        <div className='w-full my-6'>
 
-          <div>
+          <div className='flex flex-wrap w-full gap-4'>
 
-            <div className='flex flex-col gap-2 border w-full p-4 rounded border-primary-700 text-gray-600'>
-              <p className='font-semibold text-base'>Customers</p>
+            <div className='flex flex-col w-full gap-2 border p-4 rounded border-primary-700 text-gray-600 md:w-60'>
+              <p className='font-semibold text-base'>Current Stock</p>
               <p className='text-lg font-semibold text-black'>12,000</p>
               <p className='text-sm text-gray-400'>Since this month</p>
             </div>
 
-            <div className='flex flex-col gap-2 border w-full p-4 rounded border-primary-700 text-gray-600'>
-              <p className='font-semibold text-base'>Customers</p>
+            <div className='flex flex-col w-full gap-2 border p-4 rounded border-primary-700 text-gray-600 md:w-60'>
+              <p className='font-semibold text-base'>Gas Type & QTY</p>
+              <p className='text-lg font-semibold text-black'>S - 100 | M - 200 | L - 280</p>
+              <p className='text-sm text-gray-400'>Since this month</p>
+            </div>
+
+            <div className='flex flex-col w-full gap-2 border p-4 rounded border-primary-700 text-gray-600 md:w-60'>
+              <p className='font-semibold text-base'>Gas Request</p>
               <p className='text-lg font-semibold text-black'>12,000</p>
               <p className='text-sm text-gray-400'>Since this month</p>
             </div>
 
-            <div className='flex flex-col gap-2 border w-full p-4 rounded border-primary-700 text-gray-600'>
-              <p className='font-semibold text-base'>Customers</p>
+            <div className='flex flex-col w-full gap-2 border p-4 rounded border-primary-700 text-gray-600 md:w-60'>
+              <p className='font-semibold text-base'>Stock Requests</p>
               <p className='text-lg font-semibold text-black'>12,000</p>
               <p className='text-sm text-gray-400'>Since this month</p>
             </div>
 
-            <div className='flex flex-col gap-2 border w-full p-4 rounded border-primary-700 text-gray-600'>
-              <p className='font-semibold text-base'>Customers</p>
+            <div className='flex flex-col w-full gap-2 border p-4 rounded border-primary-700 text-gray-600 md:w-60'>
+              <p className='font-semibold text-base'>Delivery Scheduled</p>
               <p className='text-lg font-semibold text-black'>12,000</p>
               <p className='text-sm text-gray-400'>Since this month</p>
             </div>
-
-            <div className='flex flex-col gap-2 border w-full p-4 rounded border-primary-700 text-gray-600'>
-              <p className='font-semibold text-base'>Customers</p>
-              <p className='text-lg font-semibold text-black'>12,000</p>
-              <p className='text-sm text-gray-400'>Since this month</p>
-            </div>w
-
           </div>
-
         </div>
+
 
       </div>
     </>
