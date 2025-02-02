7aeb40eca2eff66fda1d1b3656ff33deecc03ba1
Harishanth03 <kandeepanharishanth@gmail.com>
2025-02-02 19:22:36 +0530

    [Add] Extend BRModule schema to include image and BRNumber fields

--- backend/src/models/BRModule.js
+++ backend/src/models/BRModule.js
@@ -3,5 +3,7 @@ import mongoose from 'mongoose';
 const BRschema = new mongoose.Schema({
 
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    
-})
+    image : {type:String , required:true},
+    BRNumber:{type:Number , required:true}
+
+});
