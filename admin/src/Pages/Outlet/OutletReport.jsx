eb32486beb69f0468e95330a61563690d2598727
Harishanth03 <kandeepanharishanth@gmail.com>
2025-02-04 21:21:12 +0530

    Add form structure to OutletReport component

--- admin/src/Pages/Outlet/OutletReport.jsx
+++ admin/src/Pages/Outlet/OutletReport.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const OutletReport = () => {
   return (
     <>
-      
+      <form className='w-full max-w-[90%] md:max-w-[60%] flex flex-col text-gray-700 text-base mt-6 rounded-md py-6 px-4 md:px-6 mx-5 bg-white border'>
+        
+      </form>
     </>
   )
 }
