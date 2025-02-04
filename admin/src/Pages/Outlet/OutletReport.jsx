caa9a544d2416af909428e4e4bd5e71d8c55a823
Harishanth03 <kandeepanharishanth@gmail.com>
2025-02-04 21:40:51 +0530

    Enhance OutletReport layout with button options for Stock, Gas Stock, Gas Request, and Total Users

--- admin/src/Pages/Outlet/OutletReport.jsx
+++ admin/src/Pages/Outlet/OutletReport.jsx
@@ -7,13 +7,26 @@ const OutletReport = () => {
 
         <div className='w-full flex flex-col gap-6 mx-auto border py-6 px-6 rounded'>
 
-        <div className="flex flex-col items-center gap-4 md:items-start md:gap-5">
+          <div className="flex w-full flex-col items-center gap-4 md:items-start md:gap-5">
 
-          <h1 className='font-semibold text-2xl md:text-3xl'>
-            <span className='text-primary-700'>Outlet Report</span> Section
-          </h1>
+            <h1 className='font-semibold text-2xl md:text-3xl'>
+              <span className='text-primary-700'>Outlet Report</span> Section
+            </h1>
 
-        </div>
+            <div className='w-full flex flex-col justify-center items-center'>
+
+              <div className='p-6 bg-gray-100 rounded-md w-full flex items-end justify-center gap-2 md:gap-12'>
+
+                <button className='py-[10px] px-[18px] bg-white rounded-full'>Stock</button>
+                <button className='py-[10px] px-[18px] bg-white rounded-full'>Gas Stock</button>
+                <button className='py-[10px] px-[18px] bg-white rounded-full'>Gas Request</button>
+                <button className='py-[10px] px-[18px] bg-white rounded-full'>Total Users</button>
+
+              </div>
+
+            </div>
+
+          </div>
 
         </div>
         
