53147ee83cfe0db280eca84fa4a9cd4d6ed486f3
RenugaRajanayagam <renurajanayagam@gmail.com>
2025-02-09 23:06:17 +0530

    modified OutletMiddleWare.js

--- backend/src/middlewares/OutletMiddleWare.js
+++ backend/src/middlewares/OutletMiddleWare.js
@@ -9,7 +9,7 @@ const authOutlet = async (req, res, next) => {
 
         const Otoken = authHeader && authHeader.startsWith('Bearer ') 
 
-        ? authHeader.split(' ')[1]   
+        ? authHeader.split(' ')[1]     
   
         : req.headers.otoken; 
     
