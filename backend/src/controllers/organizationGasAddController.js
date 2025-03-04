d5d9929e2b6d5f189569ce5618ac006942820313
mathumithasrikanthan <mathumithasrikanthan@gmail.com>
2025-03-04 20:59:48 +0530

    Add organizationGasAdd controller for handling gas addition requests

---
+++ backend/src/controllers/organizationGasAddController.js
@@ -0,0 +1,25 @@
+import {v2 as cloudinary} from 'cloudinary';
+
+const organizationGasAdd = async(request , response) => {
+
+    try {
+        
+        const {gasType , weight , price , stock} = request.body;
+
+        const imageFile = request.file;
+
+        if(!gasType || !weight || !price || !stock || !imageFile){
+
+            return response.status(400).json({success: false , message : "Information are missing"});
+        }
+
+        if(stock < 0){
+            return response.status(400).json({success: false , message : "Please enter a valid stock amount"});
+        }
+
+
+    } catch (error) {
+        
+    }
+
+}
