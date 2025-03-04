8e917882c8a8ed0628e74df19ec5db947e160a82
mathumithasrikanthan <mathumithasrikanthan@gmail.com>
2025-03-04 23:10:47 +0530

    Add state management for gas type, weight, price, stock, and image in AddOrganizationGas component

--- admin/src/Pages/Admin/AddOrganizationGas.jsx
+++ admin/src/Pages/Admin/AddOrganizationGas.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddOrganizationGas = () => {
+
+  const [gasType , setType] = useState("");
+  const [weight , setWeight] = useState("");
+  const [price , setPrice] = useState("");
+  const [stock , setStock] = useState("");
+  const [gasImg , setGasImg] = useState("");
+  
   return (
     <div className="w-full flex flex-col min-h-full">
       <form className="w-full max-w-[90%] md:max-w-[60%] flex flex-col text-gray-700 text-base mt-6 rounded-md py-6 px-4 md:px-6 mx-5 bg-white border">
